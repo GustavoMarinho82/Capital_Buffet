47ce66e862b54eab7b306f96ad587f83228610de
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2023-03-25 20:43:45 -0300

    Criação do branch do back-end

--- CapitalBuffet.sql
+++ CapitalBuffet.sql
@@ -1,17 +1,18 @@
-CREATE TABLE clientes (
-	id_cliente INT UNSIGNED AUTO_INCREMENT,
-	nome_cliente VARCHAR(100) NOT NULL,
+CREATE TABLE usuarios (
+	id_usuario INT UNSIGNED AUTO_INCREMENT,
+	nome_usuario VARCHAR(100) NOT NULL,
 	cpf CHAR(14) UNIQUE,
 	cnpj CHAR(18) UNIQUE,
-	cep CHAR(9) NOT NULL UNIQUE,
-	email_cliente VARCHAR(50) NOT NULL UNIQUE,
-	telefone_cliente CHAR(12) NOT NULL, #2199988-7766
-		PRIMARY KEY (id_cliente)
+	cep CHAR(9) NOT NULL,
+	email_usuario VARCHAR(50) NOT NULL UNIQUE,
+	telefone_usuario CHAR(12) NOT NULL, #2199988-7766
+	funcionario CHAR(1) NOT NULL, #S ou N
+		PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE registros_financeiros (
 	id_registro INT UNSIGNED AUTO_INCREMENT,
-	periodo DATE NOT NULL UNIQUE, #'AAAA-MM-DD'
+	periodo DATE NOT NULL, #'AAAA-MM-DD'
 	valor DECIMAL(11, 2) NOT NULL, #123456789.10
 	descricao TEXT,
 		PRIMARY KEY (id_registro)
@@ -23,19 +24,19 @@ CREATE TABLE pedidos (
 	data_pedido DATE NOT NULL, #'AAAA-MM-DD'
 	data_evento DATE NOT NULL, #'AAAA-MM-DD'
 	endereco TEXT NOT NULL,
-	qtd_convidados SMALLINT NOT NULL,
+	qtd_convidados SMALLINT UNSIGNED NOT NULL,
 		PRIMARY KEY (id_pedido),
   
-	cliente_id INT UNSIGNED NOT NULL,
-		FOREIGN KEY (cliente_id) REFERENCES clientes (id_cliente)
+	usuario_id INT UNSIGNED NOT NULL,
+		FOREIGN KEY (usuario_id) REFERENCES usuarios (id_usuario)
 );
 
 CREATE TABLE funcionarios (
 	cpf_funcionario CHAR(14),
 	nome_funcionario VARCHAR(100) NOT NULL,
 	cargo TINYTEXT NOT NULL,
-	salario DECIMAL(11, 2) NOT NULL, #123456789.10
-	telefone_funcionario CHAR(12) NOT NULL, #2199988-7766
+	salario DECIMAL(11, 2) NOT NULL,	#123456789.10
+	telefone_funcionario CHAR(12) NOT NULL,	#2199988-7766
 	email_funcionario VARCHAR(50) NOT NULL UNIQUE,
 		PRIMARY KEY (cpf_funcionario)
 );
@@ -59,6 +60,7 @@ CREATE TABLE produtos (
 CREATE TABLE pedido_produtos (
 	pedido_id INT UNSIGNED NOT NULL,
 	produto_id INT UNSIGNED NOT NULL,
+	quantidade_p SMALLINT UNSIGNED NOT NULL,
 		FOREIGN KEY (pedido_id) REFERENCES pedidos (id_pedido),
 		FOREIGN KEY (produto_id) REFERENCES produtos (id_produto)
 );
@@ -76,6 +78,7 @@ CREATE TABLE comidas (
 CREATE TABLE pedido_comidas (
 	pedido_id INT UNSIGNED NOT NULL,
 	comida_id INT UNSIGNED NOT NULL,
+	quantidade_c SMALLINT UNSIGNED NOT NULL,
 		FOREIGN KEY (pedido_id) REFERENCES pedidos (id_pedido),
 		FOREIGN KEY (comida_id) REFERENCES comidas (id_comida)
 );
