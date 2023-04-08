cbe68cb0eacc292df17407da2f370a5a7d05dfbe
toddync <dagvjr@gmail.com>
2023-04-08 10:52:41 -0300

    making the article constructor to show the foods

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -35,61 +35,61 @@ document.querySelectorAll("l").forEach( one => {
 
     });
 })
+function set(){
+    var action = document.querySelectorAll(".material-symbols-outlined");
+    action.forEach(MP => {
 
-var action = document.querySelectorAll(".material-symbols-outlined");
-action.forEach(MP => {
-
-    if(MP.classList.contains("plus")){
-        MP.addEventListener("click", function (){
-            var number = document.getElementById("n"+this.id);
-            var counter = parseInt(number.innerHTML);
-            var name = document.getElementById(this.id+"-name").innerHTML
-            var price = parseFloat(document.getElementById(this.id+"price").getAttribute("data-price"))
-            if( document.getElementById("c"+this.id).checked && parseInt(number.getAttribute("data-limit")) > counter){
-                number.innerHTML = counter + 1;
-                communication[name] = counter + 1;
-                communication.price += price
-                console.log(JSON.stringify(communication))
-                console.log(JSON.parse(JSON.stringify(communication)))
-            }
-        });
-    } else if(MP.classList.contains("minus")){
-        MP.addEventListener("click", function (){
-            var number = document.getElementById("n"+this.id);
-            var counter = parseInt(number.innerHTML);
-            var price = parseFloat(document.getElementById(this.id+"price").getAttribute("data-price"))
-            var name = document.getElementById(this.id+"-name").innerHTML
-            if(counter >= 1 && document.getElementById("c"+this.id).checked ){
-                number.innerHTML = counter - 1;
-                communication[name] = counter - 1;
-                communication.price -= price
-                console.log(JSON.stringify(communication))
-                console.log(JSON.parse(JSON.stringify(communication)))
-            }
-        });
-    }
-
-});
-
-
-var toggle = document.querySelectorAll("input");
-toggle.forEach( toggler => {
-    if(toggler.getAttribute("data-Toggle")){
-        toggler.addEventListener("click", function (){
-            if(this.checked){
-            document.querySelectorAll("."+this.getAttribute("data-Toggle")).forEach( act => { act.classList.remove("gray")} )
-        } else {
-            document.querySelectorAll("."+this.getAttribute("data-Toggle")).forEach( act => { act.classList.add("gray")} )
-            document.getElementById("n"+this.getAttribute("data-Toggle")).innerHTML = "0";
-            var name = document.getElementById(this.getAttribute("data-Toggle")+"-name").innerHTML
-            communication[name] = 0;
-            console.log(JSON.stringify(communication))
-            console.log(JSON.parse(JSON.stringify(communication)))
+        if(MP.classList.contains("plus")){
+            MP.addEventListener("click", function (){
+                var number = document.getElementById("n"+this.id);
+                var counter = parseInt(number.innerHTML);
+                var name = document.getElementById(this.id+"-name").innerHTML
+                var price = parseFloat(document.getElementById(this.id+"price").getAttribute("data-price"))
+                if( document.getElementById("c"+this.id).checked && parseInt(number.getAttribute("data-limit")) > counter){
+                    number.innerHTML = counter + 1;
+                    communication[name] = counter + 1;
+                    communication.price += price
+                    console.log(JSON.stringify(communication))
+                    console.log(JSON.parse(JSON.stringify(communication)))
+                }
+            });
+        } else if(MP.classList.contains("minus")){
+            MP.addEventListener("click", function (){
+                var number = document.getElementById("n"+this.id);
+                var counter = parseInt(number.innerHTML);
+                var price = parseFloat(document.getElementById(this.id+"price").getAttribute("data-price"))
+                var name = document.getElementById(this.id+"-name").innerHTML
+                if(counter >= 1 && document.getElementById("c"+this.id).checked ){
+                    number.innerHTML = counter - 1;
+                    communication[name] = counter - 1;
+                    communication.price -= price
+                    console.log(JSON.stringify(communication))
+                    console.log(JSON.parse(JSON.stringify(communication)))
+                }
+            });
         }
-        })
-    }
-});
 
+    });
+
+
+    var toggle = document.querySelectorAll("input");
+    toggle.forEach( toggler => {
+        if(toggler.getAttribute("data-Toggle")){
+            toggler.addEventListener("click", function (){
+                if(this.checked){
+                document.querySelectorAll("."+this.getAttribute("data-Toggle")).forEach( act => { act.classList.remove("gray")} )
+            } else {
+                document.querySelectorAll("."+this.getAttribute("data-Toggle")).forEach( act => { act.classList.add("gray")} )
+                document.getElementById("n"+this.getAttribute("data-Toggle")).innerHTML = "0";
+                var name = document.getElementById(this.getAttribute("data-Toggle")+"-name").innerHTML
+                communication[name] = 0;
+                console.log(JSON.stringify(communication))
+                console.log(JSON.parse(JSON.stringify(communication)))
+            }
+            })
+        }
+    });
+}
 function createFood(name, price, stock, type, category, desc){
     axios.get("../..//PHP/Comidas/cadastrarComida.php", { 
         params:{
@@ -112,10 +112,11 @@ function listFood(name, category){
         categoria: type
     }}
     ).then( e => {
-        console.log(e.data)
+        document.querySelector(".card-grid").innerHTML = articleCreate(e.data);
+        set();
     }).catch();
 }
-
+listFood();
 function delFood(id){
     axios.get("../..//PHP/Comidas/deletarComida.php", { params:{
             id:id
@@ -138,4 +139,39 @@ function modFood(id, name, price, stock, type, category, desc){
     }}).then( e => {
         console.log(e)
     }).catch();
+}
+
+
+function articleCreate(content){
+    var response = "";
+    Object.keys(content).forEach(function(key, index){
+        if (typeof(content[index].nome) == "undefined" || content[index].estoque == "0" ){
+        
+        }else{
+        response += `
+        <article class="card" data-Type="${content[index].categoria}">
+			<div class="card-header">
+				<div>
+					<span></span>
+					<h3 id="_${content[index].id}-name">${content[index].nome}</h3>
+				</div>
+				<label class="toggle">
+					<input type="checkbox" id="c_${content[index].id}" data-Toggle="_${content[index].id}">
+					<span></span>
+				</label>
+			</div>
+			<div class="card-body">
+        <img src="${content[index].img || "../imagens/logo.png"}"/>
+			</div>
+			<div class="card-footer">
+				<a href="#">Quantity:</a><hr>
+				<span id="_${content[index].id}" class="material-symbols-outlined minus _${content[index].id} gray">do_not_disturb_on</span>
+				<number data-limit="${content[index].estoque}" id="n_${content[index].id}">0</number>
+				<span id="_${content[index].id}" class="material-symbols-outlined plus _${content[index].id} gray">add_circle</span>
+			</div>
+			<div id ="_${content[index].id}price" data-price="${content[index].preco}" class="price">Price: R$ ${content[index].preco}</div>
+	    </article>`;
+    }
+    });
+    return response;
 }
--- PHP/Comidas/listarComidas.php
+++ PHP/Comidas/listarComidas.php
@@ -40,16 +40,16 @@ if(isset($_GET)){
 }
 $consulta = mysqli_query($mysqli, $sql);
 $reuturn = array();
-$x = 1;
+$x = 0;
     while ($linha  = mysqli_fetch_array($consulta)) {
-        $return[$x] = array(
-            "id" => $linha ['id_comida'],
-            "nome" => $linha["nome_comida"],
-            "preco" => $linha['preco_comida'], "estoque" => $linha ['estoque_comida'],
-            "tipo" => $linha['tipo'],
-            "categoria" => $linha [ 'categoria'],
-            "descricao" => $linha ['descricao_comida']
-        );
-        $x++;
+            $return[$x] = array(
+                "id" => $linha ['id_comida'],
+                "nome" => $linha["nome_comida"],
+                "preco" => $linha['preco_comida'], "estoque" => $linha ['estoque_comida'],
+                "tipo" => $linha['tipo'],
+                "categoria" => $linha [ 'categoria'],
+                "descricao" => $linha ['descricao_comida']
+            );
+            $x++;
     }
 echo json_encode($return);
