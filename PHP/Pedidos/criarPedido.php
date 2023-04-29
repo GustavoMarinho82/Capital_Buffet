396d21620611b255aa17c7e0f91bc821c039a9c9
Gustavo Marinho <gustavo.mariinho06@gmail.com>
2023-04-29 12:06:32 -0300

    Atualizando funções e arquivos de integração
    Foram atualizadas as funções: modUsuarios, criarPedido, listarPedido, listarPedido2, modPedido e delPedido.
    
    Também foram alterados os arquivos relacionados a essas funções, exceto os de listar e deletar pedidos.

--- PHP/Pedidos/criarPedido.php
+++ PHP/Pedidos/criarPedido.php
@@ -11,18 +11,16 @@ include($include . "conexao.php");
 include($include . "CORS.php");
 cors();
 
-    $tipo = $_GET['tipo_evento'];
-    $orcamento = $_GET['custo_total'];
-    $inicio_evento = $_GET['inicio_evento'];
-    $fim_evento = $_GET['fim_evento'];
+    $tipo = $_GET['tipo'];
+    $orcamento = $_GET['orcamento'];
+    $inicio = $_GET['inicio'];
+    $fim = $_GET['fim'];
     $qtd_convidados = $_GET['qtd_convidados'];
     $endereco = $_GET['endereco'];
     $observacoes = $_GET['observacoes'];
 
     $qtd_comidas = $_GET['qtd_comidas'];
     $qtd_utilitarios = $_GET['qtd_utilitarios'];
-
-    $cargos = array("Chefe de cozinha", "Ajudante de cozinha", "Copeiro", "Garçom", "Barman", "Recepcionista", "Segurança", "Faxineiro");
     $qtd_cargos = $_GET['qtd_cargos'];
 
     $usuario_id = $_GET["id_usuario"];
@@ -30,52 +28,34 @@ cors();
 
     //PEDIDOS
     $sql = "INSERT INTO pedidos (tipo_evento, orcamento, inicio_evento, fim_evento , qtd_convidados, endereco, observacoes, usuario_id) VALUES 
-        ('$tipo', $orcamento, '$inicio_evento', '$fim_evento', $qtd_convidados, '$endereco', '$observacoes', $usuario_id)";
+        ('$tipo', $orcamento, '$inicio', '$fim', $qtd_convidados, '$endereco', '$observacoes', $usuario_id)";
             mysqli_query($mysqli, $sql);
 
                 $ultimo_id = mysqli_insert_id($mysqli);
 
 
     //PEDIDOS_COMIDAS
-    $id_comida = 0; //Tem que começar por 0 porque os arrays das quantidades começam com 0 (que não referencia nenhum id)
-
-        foreach($qtd_comidas as $qtd_comida) {
-
-            if ($qtd_comida > 0) {
-                $sql = "INSERT INTO pedido_comidas (pedido_id, comida_id, qtd_comida) VALUES ($ultimo_id, $id_comida, $qtd_comida)";
-                    mysqli_query($mysqli, $sql);
-            }
-            $id_comida++;
-        }
+    foreach($qtd_comidas as $id_comida => $qtd_comida) {
+        $sql = "INSERT INTO pedido_comidas (pedido_id, comida_id, qtd_comida) VALUES ($ultimo_id, $id_comida, $qtd_comida)";
+            mysqli_query($mysqli, $sql);
+    }
 
 
-    //PEDIDOS_PRODUTOS
-    $id_utilitario = 0; 
-
-        foreach($qtd_utilitarios as $qtd_utilitario) {
-
-            if ($qtd_utilitario > 0) {
-                $sql = "INSERT INTO pedido_utilitarios (pedido_id, utilitario_id, qtd_utilitario) VALUES ($ultimo_id, $id_utilitario, $qtd_utilitario)";
-                    mysqli_query($mysqli, $sql);
-            }
-            $id_utilitario++;
-        }
+    //PEDIDOS_UTILITÁRIOS
+    foreach($qtd_utilitarios as $id_utilitario => $qtd_utilitario) {
+        $sql = "INSERT INTO pedido_utilitarios (pedido_id, utilitario_id, qtd_utilitario) VALUES ($ultimo_id, $id_utilitario, $qtd_utilitario)";
+            mysqli_query($mysqli, $sql);
+    }
 
 
     //PEDIDOS_FUNCIONARIOS
-            
-        //Ordem do array cargos -> ("Chefe de cozinha", "Ajudante de cozinha", "Copeiro", "Garçom", "Barman", "Recepcionista", "Segurança", "Faxineiro")
-                    
     $sql = "INSERT INTO pedido_funcionarios (pedido_id, funcionario_cpf) SELECT $ultimo_id, cpf_funcionario FROM funcionarios WHERE cargo=? ORDER BY rand() LIMIT ?";
         $stmt = mysqli_prepare($mysqli, $sql);
             mysqli_stmt_bind_param($stmt, "si", $cargo, $qtd_cargo);
-
-                            
-    for($x = 0; $x < count($cargos); $x++) {
-        $cargo = $cargos[$x];
-        $qtd_cargo = $qtd_cargos[$x];
-                        
-            mysqli_stmt_execute($stmt);
+          
+    foreach($qtd_cargos as $cargo => $qtd_cargo) {
+        mysqli_stmt_execute($stmt);
     }
 
 echo json_encode(array("status"=>"sucesso"));
+?>
