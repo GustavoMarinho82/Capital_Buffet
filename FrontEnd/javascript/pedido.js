bfbfab15dfedbe6551503e874741f713827673a4
Henrique <henriquenunesrj09@gmail.com>
2023-07-16 12:23:16 -0300

    teste parte2

--- FrontEnd/javascript/pedido.js
+++ FrontEnd/javascript/pedido.js
@@ -11,7 +11,40 @@ function hidePopup() {
   popupOverlay.style.display = 'none';
 }
 
+//funções CRUD
+listarPedidos()
+listarPedidos2()
 
+function criar(){
+  var tipo = document.getElementById("tipo").innerHTML;
+  var orcamento = document.getElementById("orca").innerHTML
+  var data = document.getElementById("data").innerHTML
+  var numconvidado = document.getElementById("numconvidado").innerHTML
+  var endereco = document.getElementById("endereco").innerHTML
+  var observacao = document.getElementById("obs").innerHTML
+
+  var e = criarPedido( tipo, orcamento, data, data, numconvidado, endereco, observacao, 0, 0, 0, 0);
+
+  e.then( o => {
+    if ( o.data == "Pedido cadastrado com sucesso!"){
+        listarPedido();
+        notifications(o.data)
+    } else {
+        notifications(o.data, "Message--orange", "yes")
+        console.log(o.data)
+    }
+})
+}
+
+function del(){}
+function alterar(){}
+
+
+
+idPedido = 1;
+function redirecionar(){
+  window.location.href = "../views/comidas_cliente.html?idPedido=" + idPedido;
+}
 
 
 
